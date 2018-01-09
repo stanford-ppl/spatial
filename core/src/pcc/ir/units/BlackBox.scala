d5766be2252f9a00ed0e265f1bf6f29c2a252473
David Koeplinger <dkoeplinger@gmail.com>
2018-01-08 17:42:57 -0800

    Update BlackBox

--- core/src/pcc/ir/units/BlackBox.scala
+++ core/src/pcc/ir/units/BlackBox.scala
@@ -24,8 +24,14 @@ case class CONVBox() extends BlackBox
 case class SHIFTBox(validAfter: Int) extends BlackBox // Shift out registers, valid after X cycles
 
 object BlackBox {
-  @api def GEMM[T:Num](a: SRAM[T], b: SRAM[T]): Void = stage(GEMMBox(a,b))
-
+  @api def GEMM[T:Num](
+    a: SRAM[T],
+    b: SRAM[T],
+    c: SRAM[T],
+    i: I32,
+    j: I32,
+    p: I32
+  ): Void = stage(GEMMBox(a,b,c,i,j,p))
 
   @api def GEMV: Void = stage(GEMVBox())
   @api def CONV: Void = stage(CONVBox())

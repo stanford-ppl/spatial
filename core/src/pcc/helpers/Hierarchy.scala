66cf22c2fbe36397f3e7781c3f9bb37db80bcba5
David Koeplinger <dkoeplinger@gmail.com>
2018-02-14 13:48:44 -0800

    Refactor helper functions

---
+++ core/src/pcc/helpers/Hierarchy.scala
@@ -0,0 +1,34 @@
+package pcc.helpers
+
+import forge._
+import pcc.core._
+import pcc.data._
+import pcc.node._
+
+trait Hierarchy {
+  def isControl(sym: Sym[_]): Boolean = sym.op.exists(isControl)
+  def isControl(op: Op[_]): Boolean = op.isInstanceOf[Control]
+
+  def isPrimitive(sym: Sym[_]): Boolean = sym.op.exists(isPrimitive)
+  def isPrimitive(op: Op[_]): Boolean = op.isInstanceOf[Primitive[_]]
+
+  def isStateless(sym: Sym[_]): Boolean = sym.op.exists(isStateless)
+  def isStateless(op: Op[_]): Boolean = op match {
+    case p: Primitive[_] => p.isStateless
+    case _ => false
+  }
+
+  def isAccel(sym: Sym[_]): Boolean = sym.op.exists(isAccel)
+  def isAccel(op: Op[_]): Boolean = op.isInstanceOf[AccelScope]
+
+  def isLoop(sym: Sym[_]): Boolean = sym.op.exists{case _:Loop => true; case _ => false}
+  def isLoop(ctrl: Ctrl): Boolean = isLoop(ctrl.sym)
+
+  def isSwitch(sym: Sym[_]): Boolean = sym.op.exists(isSwitch)
+  def isSwitch(op: Op[_]): Boolean = false  // TODO
+
+  @api def isInnerControl(sym: Sym[_]): Boolean = isControl(sym) && !isOuter(sym)
+  @api def isInnerControl(ctrl: Ctrl): Boolean = isInnerControl(ctrl.sym)
+  @api def isOuterControl(sym: Sym[_]): Boolean = isControl(sym) && isOuter(sym)
+  @api def isOuterControl(ctrl: Ctrl): Boolean = isOuterControl(ctrl.sym)
+}
---
+++ core/src/pcc/util/ScalaImplicits.scala
@@ -0,0 +1,59 @@
+package pcc.util
+
+/**
+  * General helper methods for various data structures in Scala.
+  */
+object ScalaImplicits {
+
+  implicit class SeqHelpers[A](x: Seq[A]) {
+    def get(i: Int): Option[A] = if (i >= 0 && i < x.length) Some(x(i)) else None
+    def indexOrElse(i: Int, els: => A): A = if (i >= 0 && i < x.length) x(i) else els
+
+    /**
+      * Returns true if the length of x is exactly len, false otherwise.
+      * Equivalent to (but faster than) x.length == len
+      */
+    def lengthIs(len: Int): Boolean = x.lengthCompare(len) == 0
+
+    /**
+      * Returns true if length of x is less than len, false otherwise.
+      * Equivalent to (but faster than) x.length < len
+      */
+    def lengthLessThan(len: Int): Boolean = x.lengthCompare(len) < 0
+
+    /**
+      * Returns true if length of x is more than len, false otherwise
+      * Equivalent to (but faster than) x.length > len
+      */
+    def lengthMoreThan(len: Int): Boolean = x.lengthCompare(len) > 0
+
+  }
+
+  implicit class IterableHelpers[A](x: Iterable[A]) {
+    def maxFoldBy[B:Ordering](z: A)(f: A => B): A = if (x.isEmpty) z else x.maxBy(f)
+    def minFoldBy[B:Ordering](z: A)(f: A => B): A = if (x.isEmpty) z else x.minBy(f)
+
+    def minOrZero(z: A)(implicit o: Ordering[A]): A = if (x.isEmpty) z else x.min
+    def maxOrZero(z: A)(implicit o: Ordering[A]): A = if (x.isEmpty) z else x.max
+
+    def cross[B](y: Iterable[B]): Iterator[(A,B)] = {
+      x.iterator.flatMap{a => y.iterator.map{b => (a,b) } }
+    }
+
+    /**
+      * Returns true if the given function is true over all combinations of 2 elements
+      * in this collection.
+      */
+    def forallPairs(func: (A,A) => Boolean): Boolean = x.pairs.forall{case (a,b) => func(a,b) }
+
+    /**
+      * Returns an iterator over all combinations of 2 from this iterable collection.
+      * Assumes that either the collection has (functionally) distinct elements.
+      */
+    def pairs: Iterator[(A,A)] = x match {
+      case m0 :: y => y.iterator.map{m1 => (m0,m1) } ++ y.pairs
+      case _ => Iterator.empty
+    }
+  }
+
+}

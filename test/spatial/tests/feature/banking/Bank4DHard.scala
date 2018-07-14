85dbd170ad31c361a163286d873d95d548ab22dc
Matthew Feldman <mattfel@stanford.edu>
2018-07-13 18:41:04 -0700

    add hard bakning unit test, fix hang with delayed stream valid getting halted just as fifo fills up in unaligned load (caught in SmallTensorLoadStore)

---
+++ test/spatial/tests/feature/banking/Bank4DHard.scala
@@ -0,0 +1,31 @@
+package spatial.tests.feature.banking
+
+import spatial.dsl._
+
+@spatial class Bank4DHard extends SpatialTest {
+  // Not really "hard", but used to keep ExhaustiveBanking churning for a crazy long time
+  val I = 96; val R = 3; val C = 3; val O = 96
+  val PI = 2; val PR = 3;val PC= 3; val PO = 2
+
+  def main(args: Array[String]): Unit = {
+    val dram = DRAM[Int](I,R,C,O)
+    val data = (0::I,0::R,0::C,0::O){(i,j,k,l) => (i+j+k+l) % 5}
+    setMem(dram,data)
+    val out = ArgOut[Int]
+    Accel {
+      val x = SRAM[Int](I,R,C,O)
+      x load dram
+
+      out := Reduce(Reg[Int])(0 until I par PI, 0 until R par PR, 0 until C par PC, 0 until O par PO){case List(i,r,c,o) =>
+        x(i,r,c,o)
+      }{_+_}
+    }
+
+    val gold = data.reduce{_+_}
+    val got = getArg(out)
+    println(r"Got $got, expected $gold")
+    println(r"PASS: ${got == gold}")
+    assert(got == gold)
+  }
+}
+

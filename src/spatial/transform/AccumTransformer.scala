ddefea9b30d3683dc77d21b90a9e40df14e27470
David Koeplinger <dkoeplinger@gmail.com>
2018-07-20 12:35:19 -0700

    accum specialization wip

--- src/spatial/transform/AccumTransformer.scala
+++ src/spatial/transform/AccumTransformer.scala
@@ -4,10 +4,37 @@ import argon._
 import argon.transform.MutateTransformer
 import spatial.lang._
 import spatial.node._
+import spatial.metadata.control._
+import spatial.metadata.retiming._
 import spatial.traversal.AccelTraversal
 
 case class AccumTransformer(IR: State) extends MutateTransformer with AccelTraversal {
 
+  override def transform[A:Type](lhs: Sym[A], rhs: Op[A])(implicit ctx: SrcCtx): Sym[A] = rhs match {
+    case AccelScope(_) => inHw{ transformControl(lhs,rhs) }
+    case _ => transformControl(lhs,rhs)
+  }
 
+  def transformControl[A:Type](lhs: Sym[A], rhs: Op[A]): Sym[A] = rhs match {
+    case ctrl: Control[_] if !(lhs.isSwitch && lhs.isInnerControl) =>
+      ctrl.bodies.foreach{body =>
+        if (body.isInnerStage || lhs.isInnerControl) {
+          body.blocks.foreach{case (_, block) =>
+            register(block -> optimizeAccumulators(block))
+          }
+        }
+      }
+      super.transform(lhs,rhs)
+    case _ => super.transform(lhs,rhs)
+  }
+
+
+  def optimizeAccumulators[R](block: Block[R]): Block[R] = {
+    val stms = block.stms
+    val cycleStms = stms.filter(_.isInCycle).filter(_.reduceCycle.shouldSpecialize).toSet
+
+
+
+  }
 
 }
--- src/spatial/traversal/AccumAnalyzer.scala
+++ src/spatial/traversal/AccumAnalyzer.scala
@@ -16,19 +16,30 @@ case class AccumAnalyzer(IR: State) extends AccelTraversal {
   override def visitBlock[R](block: Block[R]): Block[R] = {
     if (inHw) {
       // Find standard write-after-read accumulation candidates
+      // TODO: May want to keep this in metadata from a different common traversal
       val (_, cycles) = latenciesAndCycles(block)
       val warCycles = cycles.collect{case cycle:WARCycle => cycle }
 
-      var distinctCycles: Set[WARCycle] = Set.empty
+      var disjointCycles: Set[WARCycle] = Set.empty
 
+      // Find sets of cycles which are entirely disjoint
       warCycles.foreach{c1 =>
-        val overlapping = distinctCycles.filter{c2 => (c1.symbols intersect c2.symbols).nonEmpty }
-        if (overlapping.isEmpty) distinctCycles += c1
-        else distinctCycles --= overlapping
+        dbgs(s"Candidate cycle: ")
+
+        val overlapping = disjointCycles.filter{c2 => (c1.symbols intersect c2.symbols).nonEmpty }
+        val closed = c1.symbols.forall{s =>
+          dbgs(s"  $s")
+          val outsideConsumers = s.consumers diff c1.symbols
+          outsideConsumers.isEmpty
+        }
+        if (overlapping.isEmpty) disjointCycles += c1
+        else disjointCycles --= overlapping
       }
 
-
-
+      disjointCycles.foreach{c =>
+        val cycle = c.copy(shouldSpecialize = true)
+        c.symbols.foreach{s => s.reduceCycle = cycle }
+      }
     }
     super.visitBlock(block)
   }

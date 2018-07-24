10d0b12bee9866d1c4ff40f3e719b462b572622b
David Koeplinger <dkoeplinger@gmail.com>
2018-07-24 16:10:43 -0700

    Finish accumulation optimization

--- src/spatial/metadata/retiming/RetimingData.scala
+++ src/spatial/metadata/retiming/RetimingData.scala
@@ -13,6 +13,7 @@ abstract class Cycle extends Data[Cycle](Transfer.Remove) {
   def length: Double
   def symbols: Set[Sym[_]]
   def shouldSpecialize: Boolean
+  def cycleID: Int
 }
 
 /** Write-after-read (WAR) cycle: Standard read-accumulate loop. */
@@ -22,13 +23,15 @@ case class WARCycle(
     memory: Sym[_],
     symbols: Set[Sym[_]],
     length: Double,
-    shouldSpecialize: Boolean = false)
+    shouldSpecialize: Boolean = false,
+    cycleID: Int = -1)
   extends Cycle
 
 /** Access-after-access (AAA) cycle: Time-multiplexed reads/writes. */
 case class AAACycle(accesses: Set[Sym[_]], memory: Sym[_], length: Double) extends Cycle {
   def symbols: Set[Sym[_]] = accesses
   def shouldSpecialize: Boolean = false
+  def cycleID: Int = -1
 }
 
 

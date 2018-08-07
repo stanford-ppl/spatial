4d570171bab2372c79bef8b7dc093fd86bfa41df
Matthew <mattfel@stanford.edu>
2018-08-07 16:50:49 -0700

    fix templates crashing

--- resources/synth/chisel-templates/tests/templates/Accum.scala
+++ resources/synth/chisel-templates/tests/templates/Accum.scala
@@ -12,21 +12,21 @@ class FixFMAAccumTests(c: FixFMAAccum) extends PeekPokeTester(c) {
   var count = 0
 
   for (iter <- 0 until numResets) {
-    poke(c.io.reset, 1)
+    poke(c.io.reset(0), 1)
     step(1)
-    poke(c.io.reset, 0)
+    poke(c.io.reset(0), 0)
     step(1)
 
     count = 0
     for(i <- 0 until 10) {
       val next = rnd.nextInt(10)
       count = next * next + count
-      poke(c.io.input1, next)
-      poke(c.io.input2, next)
-      poke(c.io.enable, 1)
+      poke(c.io.input1(0), next)
+      poke(c.io.input2(0), next)
+      poke(c.io.enable(0), 1)
       step(1)
     }
-    poke(c.io.enable, 0)
+    poke(c.io.enable(0), 0)
     step(c.fmaLatency.toInt + Utils.log2Up(c.cycleLatency.toInt)) // Should be sufficiently long for drain
     val r = peek(c.io.output)
     Console.println(s"got $r, expected $count")

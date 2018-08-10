e3d38a38ac9b7885f4c3e40026119acb3672753a
David Koeplinger <dkoeplinger@gmail.com>
2018-08-09 18:55:39 -0700

    Fix Array nodes not having binds correctly defined

--- argon/src/argon/node/Series.scala
+++ argon/src/argon/node/Series.scala
@@ -8,4 +8,5 @@ import forge.tags._
 @op case class SeriesForeach[A:Num](start: Num[A], end: Num[A], step: Num[A], func: Lambda1[A,Void])
        extends Op[Void] {
   val A: Num[A] = Num[A]
+  override def binds = super.binds + func.input
 }

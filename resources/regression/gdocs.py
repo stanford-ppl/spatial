8231bcbe0e5e9b5823e1ceb195d0407a1fedc151
Matthew <mattfel@stanford.edu>
2018-08-27 10:32:26 -0700

    make gdocs report numthreads everywhere

--- resources/regression/gdocs.py
+++ resources/regression/gdocs.py
@@ -401,7 +401,7 @@ def prepare_sheet(hash, apphash, timestamp, backend):
 				# worksheet = sh.get_worksheet(x)
 				worksheet = sh.worksheet('index', x)
 				if (worksheet.title != "STATUS" and worksheet.title != "Properties"):
-					worksheet.insert_rows(row = 2, values = [link, alink, t, freq + ' MHz', os.uname()[1] ])
+					worksheet.insert_rows(row = 2, values = [link, alink, t, freq + ' MHz (' + numthreads + " threads)", os.uname()[1] ])
 					if (not keep_row_75): deleteRows(worksheet, 75)
 					# worksheet.update_cell(id,1, link)
 					# worksheet.update_cell(id,2, alink)

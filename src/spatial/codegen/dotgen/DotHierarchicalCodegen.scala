0a75117209dc2b9af8fcaf6d97934f0c2b7b86e7
Yaqi Zhang <hsbzyq@gmail.com>
2018-08-31 19:25:56 -0700

    refactor and bug fixes to edges in hiearchical codegen

--- src/spatial/codegen/dotgen/DotHierarchicalCodegen.scala
+++ src/spatial/codegen/dotgen/DotHierarchicalCodegen.scala
@@ -13,60 +13,6 @@ trait DotHierarchicalCodegen extends DotCodegen {
 
   override def clearGen = {} // prevent clear flatGen
 
-  case class Scope(sym:Option[Sym[_]]) {
-    val nodes = mutable.ListBuffer[Sym[_]]()
-    val edges = mutable.Map[(String, String), Map[String,String]]()
-    val fileName = sym match {
-      case Some(sym) => src"$sym"
-      case None => s"Top"
-    }
-    val svgpath = s"${out}${files.sep}$fileName.svg"
-    def addEdge(from:Sym[_], to:Sym[_], fromAlias:String, toAlias:String):Unit = {
-      edges += ((fromAlias, toAlias) -> edgeAttr(from, to))
-    }
-
-    // called when enter a new scope
-    def begin(block: => Unit) = {
-      enter {
-        open(src"digraph G {")
-        block
-        nodes.foreach { sym => sym.op.foreach { op => emitInputs(sym) } }
-      }
-    }
-
-    // called when all scopes are done
-    def end = {
-      // Avoid emitint duplicated edges between nodes
-      enter {
-        edges.foreach { case ((from, to), attr) => 
-          emitEdge(from, to, attr)
-        }
-        close(src"}")
-      }
-    }
-
-    def enter(block: => Unit) = {
-      stack.push(this)
-      inGen(out, s"${fileName}.dot") {
-        block
-      }
-      stack.pop
-    }
-  }
-
-  val stack = mutable.Stack[Scope]()
-  def currScope = stack.head
-  def nodes = currScope.nodes
-  val scopes = mutable.Map[Option[Sym[_]],Scope]()
-  def scope(sym:Option[Sym[_]]) = scopes.getOrElseUpdate(sym, Scope(sym))
-
-  override protected def emitEntry(block: Block[_]): Unit = {
-    scope(None).begin {
-      gen(block)
-    }
-    scopes.values.foreach { scope => scope.end }
-  }
-
   override protected def gen(lhs: Sym[_], rhs: Op[_]): Unit = {
     nodes += lhs
     emitNode(lhs)
@@ -75,8 +21,7 @@ trait DotHierarchicalCodegen extends DotCodegen {
         open(src"subgraph cluster_${lhs} {")
           emit(src"""${graphAttr(lhs).map { case (k,v) => s"$k=$v" }.mkString(" ")}""")
           //strMeta(lhs)
-          val bounds = rhs.binds.filter(_.isBound)
-          bounds.foreach{ b =>
+          rhs.binds.filter(_.isBound).foreach{ b =>
             nodes += b
             emitNode(b)
           }
@@ -108,7 +53,7 @@ trait DotHierarchicalCodegen extends DotCodegen {
     (shared, branchA.filterNot(shared.contains(_)), branchB.filterNot(shared.contains(_)))
   }
 
-  override def emitEscapeEdge(from:Sym[_], to:Sym[_], fromAlias:String, toAlias:String) = {
+  override def addEdge(from:Sym[_], to:Sym[_], fromAlias:String, toAlias:String) = {
     if (!nodes.contains(from)) dbgblk(src"emitEscapeEdge($from, $to):"){
       val (shared, fromBranch, toBranch) = ancestryBetween(from, to)
       dbgs(src"from=$from")
@@ -117,27 +62,20 @@ trait DotHierarchicalCodegen extends DotCodegen {
       dbgs(src"fromBranch=$fromBranch")
       dbgs(src"toBranch=$toBranch")
       toBranch.filterNot{ sym => sym == to }.foreach { ancestor =>
-        scope(Some(ancestor)).enter {
-          emitNode(from)
-          emitEdge(from, to, src"$from", src"${toBranch(toBranch.indexOf(ancestor)-1)}")
-        }
+        scope(Some(ancestor))
+          .addExternNode(from)
+          .addEdge(from, to, src"$from", src"${toBranch(toBranch.indexOf(ancestor)-1)}")
       }
       fromBranch.filterNot{ sym => sym == from }.foreach { ancestor =>
-        scope(Some(ancestor)).enter {
-          emitNode(to)
-          emitEdge(from, to, src"${fromBranch(fromBranch.indexOf(ancestor)-1)}", src"$to")
-        }
+        scope(Some(ancestor))
+          .addExternNode(to)
+          .addEdge(from, to, src"${fromBranch(fromBranch.indexOf(ancestor)-1)}", src"$to")
       }
-      scope(shared.headOption).enter {
-        emitEdge(from, to, src"${fromBranch.last}", src"${toBranch.last}")
-      }
+      scope(shared.headOption)
+        .addEdge(from, to, src"${fromBranch.last}", src"${toBranch.last}")
     } else {
-      emitEdge(from, to, fromAlias, toAlias)
+      currScope.addEdge(from, to, fromAlias, toAlias)
     }
-  }
-
-  override def emitEdge(from:Sym[_], to:Sym[_], fromAlias:String, toAlias:String):Unit = {
-    currScope.addEdge(from, to, fromAlias, toAlias)
   }
 
   override def graphAttr(lhs:Sym[_]):Map[String,String] = lhs match {

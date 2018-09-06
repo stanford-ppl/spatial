5f1d90aa5f35589bf69d7c5db66a292f8421e0f2
Yaqi Zhang <hsbzyq@gmail.com>
2018-09-05 17:46:24 -0700

    change link to relative path

--- src/spatial/codegen/dotgen/HtmlIRGen.scala
+++ src/spatial/codegen/dotgen/HtmlIRGen.scala
@@ -1,6 +1,7 @@
 package spatial.codegen.dotgen
 
 import argon._
+import utils.io.files._
 
 trait HtmlIRCodegen extends argon.codegen.Codegen {
 
@@ -10,6 +11,9 @@ trait HtmlIRCodegen extends argon.codegen.Codegen {
 
   protected def emitEntry(block: Block[_]): Unit = {
     ret(block)
+  }
+  override def clearGen(): Unit = {
+    deleteFiles(s"$out$sep$entryFile")
   }
 
   override protected def postprocess[R](block: Block[R]): Block[R] = {

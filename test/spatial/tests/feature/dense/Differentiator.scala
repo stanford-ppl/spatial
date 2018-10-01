c29190e8006284457830e0d178d21ce734fb19f1
Matthew Feldman <mattfel@stanford.edu>
2018-10-01 12:47:42 -0700

    updates to apps to match new tutorial

--- test/spatial/tests/feature/dense/Differentiator.scala
+++ test/spatial/tests/feature/dense/Differentiator.scala
@@ -3,35 +3,62 @@ package spatial.tests.feature.dense
 import spatial.dsl._
 
 @spatial class Differentiator extends SpatialTest {
-  type T = FixPt[TRUE,_16,_16]
+
+  // Set low pass filter window size
+  val window: scala.Int = 16
+
+  def compute_kernel[T:Num](start: scala.Int, sr: RegFile1[T]): T = {
+    Reduce(Reg[T](0.to[T]))(window/2 by 1) { k => sr(k+start) }{_+_} / window
+  }
 
   def main(args: Array[String]): Unit = {
     type T = FixPt[TRUE,_16,_16]
+    
+    // Set tile size
     val coltile = 64
+    
+    // Load data
     val data = loadCSV1D[T](s"$DATA/slac/slacsample1d.csv", ",")
+    
+    // Set full size of input vector for use by FPGA
     val memcols = ArgIn[Int]
     setArg(memcols, data.length.to[Int])
+    
+    // Create input and output DRAMs
     val srcmem = DRAM[T](memcols)
     setMem(srcmem, data)
     val dstmem = DRAM[T](memcols)
 
-    val window = 16
-
     Accel {
-      val sr = RegFile[T](1,window)
+      // Create shift register window
+      val sr = RegFile[T](window)
+    
+      // Allocate memories for input and output data
       val rawdata = SRAM[T](coltile)
       val results = SRAM[T](coltile)
-      Foreach(window by 1) { i => results(i) = 0} // Temporary fix to regression screwing up write to address 1 only
-      // Work on each tile of a row
+
+      // Work tile by tile on input vector
       Foreach(memcols by coltile) { c =>
+        
+        // Fetch this tile
         rawdata load srcmem(c::c+coltile)
+                                   
         // Scan through tile to get deriv
         Foreach(coltile by 1) { j =>
-          // Pipe{sr.reset(j == 0)}
-          sr(0,*) <<= rawdata(j)
-          val mean_right = Reduce(Reg[T](0.to[T]))(window/2 by 1) { k => sr(0,k) }{_+_} / window.to[T]
-          val mean_left = Reduce(Reg[T](0.to[T]))(window/2 by 1) { k => sr(0,k+window/2) }{_+_} / window.to[T]
+        
+          // Shift next element into sliding window
+          sr <<= rawdata(j)
+          
+          // Compute mean of points in first half of window
+          val mean_right = compute_kernel[T](0, sr)
+                               
+          // Compute mean of points in second half of window
+          val mean_left = compute_kernel[T](window/2, sr)
+                       
+          // Subtract and average
           val slope = (mean_right - mean_left) / (window/2).to[T]
+          
+          // Store result (if all data in window is valid)
           val idx = j + c
           results(j) = mux(idx < window, 0.to[T], slope)
         }
@@ -43,8 +70,6 @@ import spatial.dsl._
     // Extract results from accelerator
     val results = getMem(dstmem)
 
-    // // Write answer for first time
-    // writeCSV1D(results, s"$DATA/slac/deriv_gold.csv", ",")
     // Read answer
     val gold = loadCSV1D[T](s"$DATA/slac/deriv_gold.csv", ",")
 
@@ -53,7 +78,7 @@ import spatial.dsl._
     val margin = 0.5.to[T]
 
     val cksum = gold.zip(results){case (a,b) => abs(a-b) < margin}.reduce{_&&_}
-    println("PASS: " + cksum + " (Differentiator) * Look into issue with addr 1 screwing up with --retiming on and --naming off but only with waveforms off in dc6db05")
     assert(cksum)
   }
 }
+

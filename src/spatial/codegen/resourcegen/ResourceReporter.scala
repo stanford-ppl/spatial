1a2129a260c726598d136f7cf08fc15b262397f1
Yaqi Zhang <hsbzyq@gmail.com>
2018-10-04 16:59:46 -0700

    update resource reporter

--- src/spatial/codegen/resourcegen/ResourceReporter.scala
+++ src/spatial/codegen/resourcegen/ResourceReporter.scala
@@ -3,6 +3,7 @@ package spatial.codegen.resourcegen
 import argon._
 import argon.codegen.FileDependencies
 import spatial.codegen.naming._
+import argon.node._
 import spatial.lang._
 import spatial.node._
 import spatial.metadata.access._
@@ -15,32 +16,131 @@ import spatial.traversal.AccelTraversal
 
 case class ResourceReporter(IR: State)  extends NamedCodegen with FileDependencies with AccelTraversal {
   override val lang: String = "reports"
-  override val ext: String = "rpt"
+  override val ext: String = "py"
 
-  override protected def emitEntry(block: Block[_]): Unit = { config.enGen = true; gen(block) }
+  override protected def emitEntry(block: Block[_]): Unit = { gen(block) }
 
   private def bitWidth(tp: Type[_]): Int = tp match {
     case Bits(bT) => bT.nbits
     case _ => -1
   }
 
-  override def emitHeader(): Unit = { config.enGen = true; super.emitHeader() }
+  var fixOp:Int = 0
+  override def emitHeader(): Unit = { 
+    super.emitHeader()
+    emit("#!/bin/python")
+    open(s"def compSize(bitWidth, dims, padding, nBuf):")
+    emit(s"size = product([d + p for d,p in zip(dims,padding)])")
+    emit(s"return size * bitWidth")
+    close(s"")
+    emit("bramSize = {} # in bits")
+    emit("regSize = {} # in bits")
+  }
 
-  override def emitFooter(): Unit = { config.enGen = true; super.emitFooter() }
+  override def emitFooter(): Unit = { 
+    emit(s"""print("total fix op": $fixOp")""")
+    super.emitFooter()
+  }
 
-  override protected def gen(lhs: Sym[_], rhs: Op[_]): Unit = rhs match {
-    case op: SRAMNew[_,_] => emit(s"${lhs}: ${bitWidth(lhs.tp.typeArgs.head)} bits x ${lhs.constDims.product} >? ${spatialConfig.sramThreshold}")
-    case op: FIFONew[_] => emit(s"${lhs}: ${bitWidth(lhs.tp.typeArgs.head)} bits x ${lhs.constDims.product} >? ${spatialConfig.sramThreshold}")
-    case op: LIFONew[_] => emit(s"${lhs}: ${bitWidth(lhs.tp.typeArgs.head)} bits x ${lhs.constDims.product} >? ${spatialConfig.sramThreshold}")
-    case op: LineBufferNew[_] => emit(s"${lhs}: ${bitWidth(lhs.tp.typeArgs.head)} bits x ${lhs.constDims.product} >? ${spatialConfig.sramThreshold}")
-    case op: RegFileNew[_,_] => emit(s"${lhs}: ${bitWidth(lhs.tp.typeArgs.head)} bits x ${lhs.constDims.product}")
-    case op: RegNew[_] => emit(s"${lhs}: ${bitWidth(lhs.tp.typeArgs.head)} bits x 1")
-    case op: FIFORegNew[_] => emit(s"${lhs}: ${bitWidth(lhs.tp.typeArgs.head)} bits x 1")
-    case op: LUTNew[_,_] => emit(s"${lhs}: ${bitWidth(lhs.tp.typeArgs.head)} bits x ${lhs.constDims.product}")
-    case op: MergeBufferNew[_] => emit(s"${lhs}: ${bitWidth(lhs.tp.typeArgs.head)} bits x ${lhs.constDims.product}")
-    case AccelScope(func)     => /*inAccel{*/ rhs.blocks.foreach{blk => gen(blk) } /*}*/
-    case _:Control[_] => rhs.blocks.foreach{blk => gen(blk)}
-    case _ => rhs.blocks.foreach{blk => gen(blk) }
+  def emitMem(lhs:Sym[_], tp:String, dims:Seq[Int], padding:Seq[Int], depth:Int) = {
+    emit(src"""${tp}Size["${lhs}"] = compSize(${bitWidth(lhs.tp.typeArgs.head)}, ${dims}, ${padding}, ${depth})""")
+  }
+
+  override protected def quoteOrRemap(arg: Any): String = arg match {
+    case p: Seq[_] => 
+      s"[${p.map(quoteOrRemap).mkString(",")}]"
+    case _ => super.quoteOrRemap(arg)
+  }
+
+  override protected def gen(lhs: Sym[_], rhs: Op[_]): Unit = {
+    rhs match {
+      case AccelScope(func) => inAccel { 
+        spatialConfig.enGen = true
+        gen(func) 
+      } 
+      case _ => 
+        if (inHw) {
+          countResource(lhs, rhs)
+        }
+        rhs.blocks.foreach{blk => gen(blk) }
+    }
+  }
+
+  def countResource(lhs: Sym[_], rhs: Op[_]): Unit = rhs match {
+    case op: SRAMNew[_,_] => 
+      emitMem(lhs, "bram", lhs.constDims, lhs.padding, lhs.instance.depth)
+    case op: FIFONew[_] => 
+      emitMem(lhs, "bram", lhs.constDims, lhs.padding, lhs.instance.depth)
+    case op: LIFONew[_] => 
+      emitMem(lhs, "bram", lhs.constDims, lhs.padding, lhs.instance.depth)
+    case op: LineBufferNew[_] => 
+      emitMem(lhs, "bram", lhs.constDims, lhs.padding, lhs.instance.depth)
+    case op: RegFileNew[_,_] => 
+      emitMem(lhs, "bram", lhs.constDims, lhs.padding, lhs.instance.depth)
+    case op: RegNew[_] => 
+      emitMem(lhs, "reg", Seq(1), Seq(0), lhs.instance.depth)
+    case op: FIFORegNew[_] => 
+      emitMem(lhs, "reg", Seq(1), Seq(0), lhs.instance.depth)
+    case op: LUTNew[_,_] => 
+      emitMem(lhs, "reg", lhs.constDims, Seq(0), 1)
+    case op: MergeBufferNew[_] => 
+      emitMem(lhs, "reg", lhs.constDims, Seq(0), 1)
+
+    case FixInv(x)            => fixOp += 1
+    case FixNeg(x)            => fixOp += 1
+    case FixAdd(x,y)          => fixOp += 1
+    case FixSub(x,y)          => fixOp += 1
+    case FixMul(x,y)          => fixOp += 1
+    case FixDiv(x,y)          => fixOp += 1
+    case FixRecip(x)          => fixOp += 1
+    case FixMod(x,y)          => fixOp += 1
+    case FixAnd(x,y)          => fixOp += 1
+    case FixOr(x,y)           => fixOp += 1
+    case FixLst(x,y)          => fixOp += 1
+    case FixLeq(x,y)          => fixOp += 1
+    case FixXor(x,y)          => fixOp += 1
+    case FixSLA(x,y)          => fixOp += 1
+    case FixSRA(x,y)          => fixOp += 1
+    case FixSRU(x,y)          => fixOp += 1
+    case SatAdd(x,y)          => fixOp += 1
+    case SatSub(x,y)          => fixOp += 1
+    case SatMul(x,y)          => fixOp += 1
+    case SatDiv(x,y)          => fixOp += 1
+    case UnbMul(x,y)          => fixOp += 1
+    case UnbDiv(x,y)          => fixOp += 1
+    case UnbSatMul(x,y)       => fixOp += 1
+    case UnbSatDiv(x,y)       => fixOp += 1
+    case FixNeq(x,y)          => fixOp += 1
+    case FixEql(x,y)          => fixOp += 1
+    case FixMax(x,y)          => fixOp += 1
+    case FixMin(x,y)          => fixOp += 1
+    //case FixToFix(x, fmt)     => fixOp += 1
+    case FixToFlt(x, fmt)     => fixOp += 1
+    case FixToText(x)         => fixOp += 1
+    case TextToFix(x, _)      => fixOp += 1
+    case FixRandom(Some(max)) => fixOp += 1
+    case FixRandom(None)      => fixOp += 1
+    case FixAbs(x)            => fixOp += 1
+    case FixFloor(x)          => fixOp += 1
+    case FixCeil(x)           => fixOp += 1
+    case FixLn(x)             => fixOp += 1
+    case FixExp(x)            => fixOp += 1
+    case FixSqrt(x)           => fixOp += 1
+    case FixSin(x)            => fixOp += 1
+    case FixCos(x)            => fixOp += 1
+    case FixTan(x)            => fixOp += 1
+    case FixSinh(x)           => fixOp += 1
+    case FixCosh(x)           => fixOp += 1
+    case FixTanh(x)           => fixOp += 1
+    case FixAsin(x)           => fixOp += 1
+    case FixAcos(x)           => fixOp += 1
+    case FixAtan(x)           => fixOp += 1
+    case FixPow(x,exp)        => fixOp += 1
+    case FixFMA(m1,m2,add)    => fixOp += 1
+    case FixRecipSqrt(x)      => fixOp += 1
+    case FixSigmoid(x)        => fixOp += 1
+
+    case _ =>
   }
 
 

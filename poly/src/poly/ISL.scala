688f879cdcdf428b4c7f08c326f95d25a3ea480d
pyprogrammer <nzhang32@gmail.com>
2018-10-24 12:10:10 -0700

    Should fix race condition on isl compile

--- poly/src/poly/ISL.scala
+++ poly/src/poly/ISL.scala
@@ -2,7 +2,6 @@ package poly
 
 import utils.process.BackgroundProcess
 
-import sys.process._
 import scala.language.postfixOps
 /**
   * Self-initializing object that runs external polyhedral ISL processes
@@ -12,36 +11,64 @@ trait ISL {
     // get path to emptiness
 
     val emptiness_bin = s"""${sys.env.getOrElse("HOME", "")}/bin/emptiness"""
-    val emptiness_exists = java.nio.file.Files.exists(java.nio.file.Paths.get(emptiness_bin))
+
+    val emptiness_lock = java.nio.file.Paths.get(emptiness_bin + ".lock")
+    val emptiness_path = java.nio.file.Paths.get(emptiness_bin)
+    val emptiness_exists = java.nio.file.Files.exists(emptiness_path)
     println(s"Emptiness: $emptiness_exists, $emptiness_bin")
 
     if (!emptiness_exists) {
-      // compile emptiness
+      // Acquire a file lock
+      val channel = java.nio.channels.FileChannel.open(emptiness_lock, java.nio.file.StandardOpenOption.CREATE)
+      val lock = channel.tryLock()
+      try {
+        if (lock == null) {
+          // Someone else is handling the compile. Wait for the file to become available.
+          println("Another process is making the emptiness bin.")
+          val wait = channel.lock()
+          wait.release()
+        } else {
+          // compile emptiness
 
-      // gcc command: CC resources/emptiness.c -o emptiness  `pkg-config --cflags --libs isl`
+          // gcc command: CC resources/emptiness.c -o emptiness  `pkg-config --cflags --libs isl`
 
-      {
-        import java.nio.file.Files
-        val create_path = java.nio.file.Paths.get(emptiness_bin).getParent
-        Files.createDirectories(create_path)
+          {
+            import java.nio.file.Files
+            val create_path = java.nio.file.Paths.get(emptiness_bin).getParent
+            Files.createDirectories(create_path)
+          }
+
+          val pkg_config_proc = BackgroundProcess("", "pkg-config", "--cflags", "--libs", "isl")
+          val pkg_config = pkg_config_proc.blockOnLine()
+
+          println(s"Pkg Config: $pkg_config")
+
+          val split_config = pkg_config.split("\\s") map {
+            _.trim
+          } filter {
+            _.nonEmpty
+          }
+
+          val compile_proc = BackgroundProcess("",
+            List(s"${sys.env.getOrElse("CC", "gcc")}", s"-xc", "-o", s"$emptiness_bin", "-") ++ split_config)
+          val source_string = scala.io.Source.fromInputStream(
+            getClass.getClassLoader.getResourceAsStream("emptiness.c")).mkString
+          println(source_string)
+          compile_proc send source_string
+          val retcode = compile_proc.waitFor()
+          println(s"Compile Retcode: $retcode")
+          compile_proc.checkErrors()
+
+          println("Finished Compiling")
+          lock.release()
+        }
+      } finally {
+        if (lock != null) {
+          lock.release()
+          channel.close()
+        }
+
       }
-
-      val pkg_config_proc = BackgroundProcess("", "pkg-config",  "--cflags", "--libs", "isl")
-      val pkg_config = pkg_config_proc.blockOnLine()
-
-      println(s"Pkg Config: $pkg_config")
-
-      val split_config = pkg_config.split("\\s") map {_.trim} filter {_.nonEmpty}
-
-      val compile_proc = BackgroundProcess("", List(s"${sys.env.getOrElse("CC", "gcc")}", s"-xc",  "-o", s"$emptiness_bin", "-") ++ split_config)
-      val source_string = scala.io.Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("emptiness.c")).mkString
-      println(source_string)
-      compile_proc send source_string
-      val retcode = compile_proc.waitFor()
-      println(s"Compile Retcode: $retcode")
-      compile_proc.checkErrors()
-
-      println("Finished Compiling")
     }
 
 

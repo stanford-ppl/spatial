a47dcebd4f829d44e0f2f321235887ae7c2be69f
Yaqi Zhang <hsbzyq@gmail.com>
2018-10-25 16:35:24 -0700

    bug fixes in struct gen

--- src/spatial/codegen/pirgen/PIRGenBit.scala
+++ src/spatial/codegen/pirgen/PIRGenBit.scala
@@ -10,15 +10,17 @@ trait PIRGenBit extends PIRCodegen {
 
   override protected def genAccel(lhs: Sym[_], rhs: Op[_]): Unit = rhs match {
     case Not(x)    => genOp(lhs)
-    case And(x,y)  => emit(src"val $lhs = $x && $y")
-    case Or(x,y)   => emit(src"val $lhs = $x || $y")
-    case Xor(x,y)  => emit(src"val $lhs = $x !== $y")
-    case Xnor(x,y) => emit(src"val $lhs = $x === $y")
+    case And(x,y)  => genOp(lhs)
+    case Or(x,y)   => genOp(lhs)
+    case Xor(x,y)  => genOp(lhs)
+    case Xnor(x,y) => genOp(lhs)
 
-    case BitRandom(None)      => emit(src"val $lhs = Bool(java.util.concurrent.ThreadLocalRandom.current().nextBoolean())")
-    case BitRandom(Some(max)) => emit(src"val $lhs = Bool(java.util.concurrent.ThreadLocalRandom.current().nextBoolean() && $max)")
-    case TextToBit(x)         => emit(src"val $lhs = Bool.from($x)")
-    case BitToText(x)         => emit(src"val $lhs = $x.toString")
+    case BitRandom(None)      => genOp(lhs)
+      //emit(src"val $lhs = Bool(java.util.concurrent.ThreadLocalRandom.current().nextBoolean())")
+    case BitRandom(Some(max)) => genOp(lhs)
+      //emit(src"val $lhs = Bool(java.util.concurrent.ThreadLocalRandom.current().nextBoolean() && $max)")
+    //case TextToBit(x)         =>
+    //case BitToText(x)         =>
     case _ => super.genAccel(lhs, rhs)
   }
 }

ca57178dd929f271d3d9f959a4a1e2b781cea765
Matthew Feldman <mattfel@stanford.edu>
2018-11-07 14:43:14 -0800

    fix for instrumenting controllers in the host

--- src/spatial/codegen/chiselgen/ChiselGenController.scala
+++ src/spatial/codegen/chiselgen/ChiselGenController.scala
@@ -23,7 +23,7 @@ trait ChiselGenController extends ChiselGenCommon {
   final private def enterCtrl(lhs: Sym[_]): Sym[_] = {
     val parent = if (controllerStack.isEmpty) lhs else controllerStack.head
     controllerStack.push(lhs)
-    if (spatialConfig.enableInstrumentation) instrumentCounters = instrumentCounters :+ (lhs, controllerStack.length)
+    if (spatialConfig.enableInstrumentation && inHw) instrumentCounters = instrumentCounters :+ (lhs, controllerStack.length)
     val cchain = if (lhs.cchains.isEmpty) "" else s"${lhs.cchains.head}"
     if (lhs.isOuterControl)      { widthStats += lhs.children.filter(_.s.get != lhs).toList.length }
     else if (lhs.isInnerControl) { depthStats += controllerStack.length }

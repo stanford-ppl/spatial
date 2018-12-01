2048f7720b9a0ad8814df0dc4989c98da8b71130
Matthew <mattfel@stanford.edu>
2018-12-01 09:44:58 -0800

    tweak cat api and make some apps use it, work on fifo reduction and stream dotproduct

--- argon/src/argon/lang/api/BitsAPI.scala
+++ argon/src/argon/lang/api/BitsAPI.scala
@@ -11,7 +11,6 @@ trait BitsAPI {
   @api def random[A:Bits]: A = Bits[A].random(None)
   @api def random[A:Bits](max: A): A = Bits[A].random(Some(max))
 
-  @api def cat(a: Vec[Bit], b: Vec[Bit], c: Vec[Bit]*): Vec[Bit] = Vec.concat((Seq(a,b) ++ c).reverse)
-  // @api def cat(a: Vec[Bit], b: Vec[Bit], c: Vec[Bit]*): Vec[Bit] = Vec.concat(Seq(a,b,c:_*))
+  @api def cat(c: Vec[Bit]*): Vec[Bit] = Vec.concat(c.reverse)
 
 }

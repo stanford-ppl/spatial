890d2c90d18ef89f81d7ee397affe3b46ad6d35a
Matthew Feldman <mattfel@stanford.edu>
2019-01-21 16:01:57 -0800

    fixes

--- src/spatial/codegen/chiselgen/ChiselGenDebug.scala
+++ src/spatial/codegen/chiselgen/ChiselGenDebug.scala
@@ -21,19 +21,19 @@ trait ChiselGenDebug extends ChiselGenCommon {
 
     case ExitIf(en) => 
     	val ens = and(en)
-	    emit(s"breakpoints(${earlyExits.length}) := ${ens} & (datapathEn).D(${lhs.fullDelay})")
+	    emit(s"Ledger.tieBreakpoint(breakpoints,${earlyExits.length}, ${ens} & (datapathEn).D(${lhs.fullDelay}))")
 	    earlyExits = earlyExits :+ lhs
 
     case AssertIf(en,cond,_) => 
     	if (inHw) {
 	    	val ens = and(en)
-	        emit(s"breakpoints(${earlyExits.length}) := ${ens} & (datapathEn).D(${lhs.fullDelay}) & ~${quote(cond)}")
+	        emit(s"Ledger.tieBreakpoint(breakpoints,${earlyExits.length}, ${ens} & (datapathEn).D(${lhs.fullDelay}) & ~${quote(cond)})")
 	        earlyExits = earlyExits :+ lhs
 	    }
 
     case BreakpointIf(en) => 
         val ens = and(en)
-        emit(s"breakpoints(${earlyExits.length}) := ${ens} & (datapathEn).D(${lhs.fullDelay})")
+        emit(s"Ledger.tieBreakpoint(breakpoints,${earlyExits.length}, ${ens} & (datapathEn).D(${lhs.fullDelay}))")
         earlyExits = earlyExits :+ lhs
 
 	case _ => super.gen(lhs, rhs)

d8f6ab24a57150c5bab26055434d95ab94c7555e
Matthew Vilim <mvilim@stanford.edu>
2019-03-12 11:51:29 -0700

    Revert "cleanup sort"
    This reverts commit f20c112abd85b0d3e14cfb3360b59770aa92af78.

--- test/spatial/tests/apps/MachSuite.scala
+++ test/spatial/tests/apps/MachSuite.scala
@@ -1819,23 +1819,33 @@ import utils.io.files._
   def main(args: Array[String]): Unit = {
 
     val numel = 2048
+    val START = 0
+    val STOP = numel
+    val levels = STOP-START //ArgIn[Int]
+    // setArg(levels, args(0).to[Int])
 
-    val value = Array.tabulate(numel) { i => numel - i - 1 }
+    val raw_data = loadCSV1D[Int](s"$DATA/sort/sort_data.csv", "\n")
 
-    val valueDRAM = DRAM[Int](numel)
-    val sortValueDRAM = DRAM[Int](2 * numel)
+    val data_dram = DRAM[Int](numel)
+    // val sorted_dram = DRAM[Int](numel)
 
-    setMem(valueDRAM, value)
+    setMem(data_dram, raw_data)
+
+    val result_dram = DRAM[Int](numel)
 
     Accel{
-      Sort.mergeSort(valueDRAM, sortValueDRAM, 16, 2, numel)
+      Sort.mergeSort(data_dram, result_dram, 16, 2, numel)
     }
 
-    val valueSort = getMem(sortValueDRAM(0::numel))
+    val sorted_gold = loadCSV1D[Int](s"$DATA/sort/sort_gold.csv", "\n")
+    val sorted_result = getMem(result_dram)
 
-    printArray(valueSort, "Sorted Result: ")
+    printArray(sorted_gold, "Sorted Gold: ")
+    printArray(sorted_result, "Sorted Result: ")
 
-     val cksum = Array.tabulate(numel - 1){ i => pack(valueSort(i), valueSort(i+1)) }.map{a => a._1 <= a._2}.reduce{_&&_}
+    val cksum = sorted_gold.zip(sorted_result){_==_}.reduce{_&&_}
+    // // Use the real way to check if list is sorted instead of using machsuite gold
+    // val cksum = Array.tabulate(STOP-1){ i => pack(sorted_result(i), sorted_result(i+1)) }.map{a => a._1 <= a._2}.reduce{_&&_}
     println("PASS: " + cksum + " (Sort_Merge)")
     assert(cksum)
   }

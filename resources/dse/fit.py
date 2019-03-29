bf3d1b7c6f6fb7a5454cbd9c561eb30e663645f5
Matthew Feldman <mattfel@stanford.edu>
2019-03-29 14:26:31 -0700

    update model

--- resources/dse/fit.py
+++ resources/dse/fit.py
@@ -40,9 +40,9 @@ def fitFunc3(x, congestion, stallPenalty, idle, startup, parFactor, a, b, c, d, 
 def fitFunc4(x, congestion, stallPenalty, idle, startup, parFactor, a, b, c, d, e, f, g, h, i, j):
 	loads, stores, gateds, outerIters, innerIters, bitsPerCycle = params(x)
 	countersContribution = outerIters * (innerIters + idle)
-	congestionContribution = (loads*a + stores*b + gateds*c) * e
+	congestionContribution = (loads*a + stores*b + gateds*c)*congestion
 	parallelizationScale = bitsPerCycle * parFactor
-	return (countersContribution * stallPenalty * congestionContribution + startup) * parallelizationScale + d
+	return (countersContribution * stallPenalty * (congestionContribution + countersContribution / bitsPerCycle * j) + startup) * parallelizationScale
 
 def fitFunc(i, data):
 	if (i == 0):       return optimize(fitFunc0, data)
@@ -64,20 +64,25 @@ def optimize(func, data):
 											  )
 		# print ' fit coefficients:\n', fitParams
 
-		avg = []
-		for x in data.transpose():
-			predict = func(x[0:6], *fitParams)
-			gold = x[col]
-			err = 100.0 * predict / gold
-			pe = abs(predict - gold) #/ gold
-			avg.append(pe)
-			# print("got: %.2f, want: %.2f, %%: %.2f" % (predict, gold, err))
-
-		st = stats.describe(avg)
-		print(st)
+		st = characterize(func, data, False, fitParams)
 		return st.mean, fitParams
 	except: 
 		return 99999999999, []
+
+def characterize(func, data, final, fitParams):
+	rawerrs = []
+	for x in data.transpose():
+		predict = func(x[0:6], *fitParams)
+		gold = x[col]
+		err = 100.0 * predict / gold
+		rawerr = abs(predict - gold) #/ gold
+		rawerrs.append(rawerr)
+		if (final): print("%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f" % ('\t'.join([str(k) for k in x[0:6]]), predict, gold, rawerr, 100*rawerr/gold, rawerr*100*rawerr/gold))
+
+	st = stats.describe(rawerrs)
+	print(st)
+	return st
+
 
 
 CSV_COLUMNS = ['loads','stores','gateds','outerIters','innerIters','bitsPerCycle','DenseLoad','DenseStore','GatedDenseStore']
@@ -102,7 +107,7 @@ for fit in targets:
 
 	# Try different models
 	print("TARGET %s" % fit)
-	for i in range(0, 4): 
+	for i in range(0, 5): 
 		sys.stdout.write("func%d: " % i)
 		(m, p) = fitFunc(i,data)
 		means.append(m)
@@ -128,8 +133,9 @@ if (len(list(set(besties))) > 1):
 	print("WARNING: DIFFERENT MODEL FOR DIFFERENT TARGETS!!")
 	print(besties)
 
+
 # Run sample data point
-x = [4,0,0,8,8,256]
+x = [2,0,0,8,8,256]
 # x = [1,2,1,1,195,512]
 # x = [2,0,0,32,512,32]
 print(x)
@@ -140,4 +146,14 @@ for (fit,p,best) in zip(targets,raw,besties):
 	elif best == 2: print(fitFunc2(x, *p))
 	elif best == 3: print(fitFunc3(x, *p))
 	else: print(fitFunc4(x, *p))
+
+
+# # Characterize
+# for (fit,p,best) in zip(targets,raw,besties):
+# 	sys.stdout.write("%s: " % fit)
+# 	if best == 0: characterize(fitFunc0, data, True, p)
+# 	elif best == 1: characterize(fitFunc1, data, True, p)
+# 	elif best == 2: characterize(fitFunc2, data, True, p)
+# 	elif best == 3: characterize(fitFunc3, data, True, p)
+# 	else: characterize(fitFunc4, data, True, p)
 

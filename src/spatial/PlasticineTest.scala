34c5f38d79c7f4ae193a7c66cc2d5cf248dd9da5
Yaqi Zhang <hsbzyq@gmail.com>
2019-04-17 23:50:01 -0700

    fixes to ArgInOut and MultiArgOut

--- src/spatial/PlasticineTest.scala
+++ src/spatial/PlasticineTest.scala
@@ -368,10 +368,12 @@ trait PlasticineTest extends DSLTest { test =>
     override def logDir(name:String):String = s"${IR.config.cwd}/gen/P14x14/$name/log"
     override def repDir(name:String):String = s"${IR.config.cwd}/gen/P14x14/$name/report"
     def runPasses():Result = {
-      genpir() >>
+      val result = genpir() >>
       pirpass("gentst", s"--mapping=true --codegen=true --net=p2p --row=$row --col=$col --tungsten --psim=false".split(" ").toList) >>
-      scommand(s"maketst", "make".split(" "), timeout=3000, parseMake, MakeError.apply, wd=IR.config.genDir+"/tungsten") >>
-      scommand(s"runtst", "./tungsten".split(" "), timeout=1000, parseTst, RunError.apply, wd=IR.config.genDir+"/tungsten")
+      scommand(s"maketst", "make".split(" "), timeout=3000, parseMake, MakeError.apply, wd=IR.config.genDir+"/tungsten")
+      runtimeArgs.cmds.foldLeft(result) { case (result, args) =>
+        result >> scommand(s"runtst", s"./tungsten ${args.mkString(" ")}".split(" "), timeout=1000, parseTst, RunError.apply, wd=IR.config.genDir+"/tungsten")
+      }
     }
   }
 

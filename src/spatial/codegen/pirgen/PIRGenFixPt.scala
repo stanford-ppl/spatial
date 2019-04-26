ee0b0245dfb6bedbbcb87e451df948f45b13b77c
Yaqi Zhang <hsbzyq@gmail.com>
2019-04-25 18:09:37 -0700

    remove VecConstNew node

--- src/spatial/codegen/pirgen/PIRGenFixPt.scala
+++ src/spatial/codegen/pirgen/PIRGenFixPt.scala
@@ -71,7 +71,6 @@ trait PIRGenFixPt extends PIRCodegen {
     case FixFMA(m1,m2,add)    => genOp(lhs)
     case FixRecipSqrt(x)      => genOp(lhs)
     case FixSigmoid(x)        => genOp(lhs)
-    case FixVecConstNew(vs) => state(lhs)(src"Const(List(${vs.mkString(",")})).tp(${lhs.sym.tp})")
 
     case _ => super.genAccel(lhs, rhs)
   }

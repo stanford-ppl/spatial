c7ff5341275156ec2e3a474f9c120b8fedacd332
Yaqi Zhang <hsbzyq@gmail.com>
2019-04-30 15:06:47 -0700

    add error message for trying to pushMultiplexedWrite for plasticine

--- src/spatial/traversal/banking/MemoryConfigurer.scala
+++ src/spatial/traversal/banking/MemoryConfigurer.scala
@@ -129,6 +129,37 @@ class MemoryConfigurer[+C[_]](mem: Mem[_,C], strategy: BankingStrategy)(implicit
 
       dbgs(s"  Unused access: ${stm(access)}")
     }
+
+    if (spatialConfig.enablePIR) {
+      instances.zipWithIndex.foreach { case (inst, dispatch) =>
+        def checkAccess(groups:Set[Set[AccessMatrix]]) = {
+          // Mapping of access matrix => group id
+          val groupMap = groups.zipWithIndex.flatMap { case (grp, gid) => grp.map { a => (a, gid) } }.toMap
+          groups.flatten.groupBy { _.access }.foreach { case (access, ams) =>
+            val gids = ams.map { a => groupMap(a) }
+            if (gids.size > 1) {
+              var msg = s"//TODO: Plasticine does not support unbanked unrolled access at the moment. "
+              msg += s"mem=$mem (${mem.ctx} ${mem.name.getOrElse("")})\n"
+              msg += s"access=$access (${access.ctx})\n"
+              msg += s"AccessMatrix:\n"
+              ams.foreach { a => 
+                msg += s"$a"
+              }
+              error(msg)
+            }
+          }
+        }
+        checkAccess(inst.reads)
+        checkAccess(inst.writes)
+      }
+      // TODO: after lid is added. check access with same outer loop uid but different inner loop
+      // uid touch the same instance
+      //used.foreach { access =>
+        //access.dispatches { case (uid, dispatches) =>
+        //}
+      //}
+    }
+
   }
 
   /** True if a and b always occur at the exact same time, or if are interface arg reads.

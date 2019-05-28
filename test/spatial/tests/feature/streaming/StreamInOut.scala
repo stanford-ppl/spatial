3b6809590de9422ca18d9e8f199d5d60c27234e2
Yaqi Zhang <hsbzyq@gmail.com>
2019-05-28 12:33:04 -0700

    revert accidentially checked in tests

--- test/spatial/tests/feature/streaming/StreamInOut.scala
+++ test/spatial/tests/feature/streaming/StreamInOut.scala
@@ -1,16 +1,15 @@
- //package spatial.tests.feature.streaming
+// package spatial.tests.feature.streaming
 
- //import spatial.dsl._
+// import spatial.dsl._
 
-//@spatial class StreamInOut extends SpatialTest {
+// object StreamInOut extends SpatialApp {
+//   import spatial.targets.DE1
 
-    //def main(args: Array[String]): Unit = {
-     //val in  = StreamIn[Tup2[Int,Bit]](FileBus[Tup2[Int,Bit]])
-     //val out = StreamOut[Tup2[Int,Bit]](FileBus[Tup2[Int,Bit]])
-     //Accel {
-       //Stream {
-         //out := in
-       //}
-     //}
-   //}
- //}
+//    def main(args: Array[String]): Unit = {
+//     val in  = StreamIn[Int](DE1.GPInput1)
+//     val out = StreamOut[Int](DE1.GPOutput1)
+//     Accel(*) {
+//       out := in
+//     }
+//   }
+// }

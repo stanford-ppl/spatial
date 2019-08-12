ce3a96a026fa95d2e03efff504095b62ce8d9476
mattfel <feldman.matthew1@gmail.com>
2019-08-12 12:49:39 -0700

    udpate to cxp

--- test/spatial/tests/feature/streaming/CXP1DFilter.scala
+++ test/spatial/tests/feature/streaming/CXP1DFilter.scala
@@ -8,8 +8,6 @@ import spatial.tests.apps._
   override def backends = DISABLED
 
 
-  val pxlBits = 16
-
 
   type T = FixPt[TRUE, _16, _16]
   val colTileSize = 512
@@ -17,10 +15,24 @@ import spatial.tests.apps._
   val deriv_window = 40
   def main(args: Array[String]): Unit = {
 
+    val deriv_window = 40
+
     // Get hard/soft derivative kernels
     val sharp_kernel = Helpers.build_derivkernel(deriv_window/8, deriv_window)
     println(r"""Kernel: ${sharp_kernel.mkString("\t")}""")
 
+
+    val rows = 99999
+    val colTileSize = 64
+    val rowTileSize = 16
+    val interleave_factor = 4
+    val bus_bits = 256
+    val line_bits = 1024*16
+    val pxl_bits = 16
+    val interleave_rows = rows/interleave_factor // Rows of packed data structure
+    val interleave_col_bits = bus_bits / interleave_factor // Bits per line per cycle
+    val interleave_cols = line_bits / interleave_col_bits // Cols of packed data structure
+    val interleave_line_els = interleave_col_bits / pxl_bits // # of elements of a line per cycle
     // /** DRAM TESTING */
     // // Get input data
     // val input_data = loadCSV2D[I16](s"$DATA/slac/xppc00117_r136_refsub_ipm4_del3.csv"," ","\n")
@@ -38,7 +50,7 @@ import spatial.tests.apps._
     /** BOARD TESTING */
     val in = StreamIn[U256](CXPPixelBus)
     val LINES_TODO = ArgIn[Int]
-    setArg(LINES_TODO, 64)
+    setArg(LINES_TODO, rows)
     val COLS = ArgIn[Int]
     setArg(COLS, 1024)
     val out = StreamOut[U256](CXPPixelBus)
@@ -46,29 +58,38 @@ import spatial.tests.apps._
     // Create HW accelerator
     Accel {
       Stream.Foreach(*){r => 
-        val input_fifo = FIFO[I16](colTileSize)
-        val issue = FIFO[Int](2*rowTileSize)
-        val result = FIFO[composite](2*rowTileSize)
+        val input_fifo = List.tabulate(interleave_factor){_ => FIFO[I16](colTileSize)}
+        val issue = FIFO[Int](2*rowTileSize).conflictable
+        val results = List.tabulate(interleave_factor){_ => FIFO[composite](2*rowTileSize/interleave_factor)}
+        val packed_results = FIFO[composite](2*rowTileSize)
 
         // Stage 1: consume cxp-like stream
         Pipe {
           val raw: U256 = in.value
-          input_fifo.enqVec(raw.asVec[I16])
+          input_fifo.zipWithIndex.foreach{case (f,i) =>
+            val raw_pack = raw.bits((i+1)*interleave_col_bits - 1 :: i*interleave_col_bits).as[I64] // U${256/interleave_col_bits}
+            f.enqVec(raw_pack.asVec[I16])
+          }
         }
 
         // Stage 2: Process (Force II = 1 to squeeze sr write and sr read into one cycle)
-        Pipe {
-          SpatialHelper.ComputeUnit[T](COLS, 1, sharp_kernel, input_fifo, issue, result, r, rowTileSize, LINES_TODO, true)
+        input_fifo.zipWithIndex.foreach{case (f,i) =>
+          Pipe{SpatialHelper.ComputeUnit[T](COLS, interleave_line_els, sharp_kernel, f, issue, results(i), r*interleave_factor + i, rowTileSize, LINES_TODO, i == (input_fifo.size-1))}
         }
 
         // Stage 3: Store
         Pipe{
           val numel = issue.deq()
-          if (numel > -1) { // Store every result since we don't care about bursting here
+          if (numel > 0) {
+            Foreach(numel / interleave_factor by 1){e =>
+              packed_results.enqVec(Vec.ZeroFirst(results.map(_.deq()):_*))
+            }
             // // DEBUG
             // deriv store deriv_fifo
             // Store results
-            out := result.deqVec(256/16).asPacked[U256]
+            Foreach(numel by 1){e =>
+              out := packed_results.deq().as[U256];
+            }
           }
         }
       }

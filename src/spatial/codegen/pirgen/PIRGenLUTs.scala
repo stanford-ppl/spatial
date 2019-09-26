4b167fecf949a096089e8cbada4ebc94f31bb25f
Yaqi Zhang <hsbzyq@gmail.com>
2019-09-25 20:32:29 -0700

    update pir, adding file lut

--- src/spatial/codegen/pirgen/PIRGenLUTs.scala
+++ src/spatial/codegen/pirgen/PIRGenLUTs.scala
@@ -7,6 +7,8 @@ import spatial.node._
 trait PIRGenLUTs extends PIRCodegen {
 
   override protected def genAccel(lhs: Sym[_], rhs: Op[_]): Unit = rhs match {
+    case op@FileLUTNew(_,filename) => 
+      stateMem(lhs, "LUT()", inits=Some(quoteString(s"file:${filename}")))
     case op@LUTNew(_,elems) => 
       stateMem(lhs, "LUT()", inits=Some(elems.map { case Const(c) => c }))
     case op@LUTBankedRead(lut,bank,ofs,ens) => 

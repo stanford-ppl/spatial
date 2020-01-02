213058376563a7e3b4d33ad6a54eacfa27ac5ec8
kelayamatoz <tzhao1309@gmail.com>
2020-01-01 20:36:28 -0800

    de1

--- fringe/src/fringe/targets/de1/DE1Interface.scala
+++ fringe/src/fringe/targets/de1/DE1Interface.scala
@@ -3,12 +3,14 @@ package fringe.targets.de1
 import chisel3._
 import fringe.globals._
 import fringe.SpatialIPInterface
-import fringe.templates.axi4.{AXI4Inlined, AXI4Probe, AvalonBundleParameters, AvalonSlave}
+import fringe.templates.axi4.{AXI4BundleParameters, AXI4Inlined, AXI4Probe, AvalonBundleParameters, AvalonSlave}
 
 class DE1Interface extends SpatialIPInterface {
   val avalonLiteParams = new AvalonBundleParameters(ADDR_WIDTH, DATA_WIDTH, 1)
   val avalonBurstParams = new AvalonBundleParameters(ADDR_WIDTH, 512, 32)
 
+  println("avalonLiteParams, addrWidth = ", avalonLiteParams.addrBits)
+  println("ADDR_WIDTH = ", ADDR_WIDTH)
   val S_AVALON = new AvalonSlave(avalonLiteParams)
   // TODO: Get the memStream working.
   val M_AXI: Vec[AXI4Inlined] =

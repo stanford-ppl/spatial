4dc1172dd8f1c6487fe75844150f02475823d0f2
mattfel <feldman.matthew1@gmail.com>
2020-05-11 10:23:43 -0700

    fix mux pushing for nonaddressable memories

--- src/spatial/metadata/memory/package.scala
+++ src/spatial/metadata/memory/package.scala
@@ -342,6 +342,7 @@ package object memory {
       }
 
     def isSingleton: Boolean = isReg || isArgIn || isArgOut || isHostIO || isFIFOReg
+    def isAddressable: Boolean = isSRAM || isLineBuffer || isRegFile || isLUT
     def isReg: Boolean = mem.isInstanceOf[Reg[_]]
     def isArgIn: Boolean = mem.isReg && mem.op.exists{ _.isInstanceOf[ArgInNew[_]] }
     def isArgOut: Boolean = mem.isReg && mem.op.exists{ _.isInstanceOf[ArgOutNew[_]] }

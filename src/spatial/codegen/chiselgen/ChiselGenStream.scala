5e703f6e71b317018dec9e58720dbe11f9b602d1
mattfel <feldman.matthew1@gmail.com>
2020-06-03 20:13:34 -0700

    fix stream read again for axi stream bus

--- src/spatial/codegen/chiselgen/ChiselGenStream.scala
+++ src/spatial/codegen/chiselgen/ChiselGenStream.scala
@@ -149,31 +149,19 @@ trait ChiselGenStream extends ChiselGenCommon {
         case _: GatherDataBus[_] => emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).r := ${strm}.bits(i).r }")
         case AxiStream256Bus(tid, tdest) if lhs.tp.typeArgs.head.isInstanceOf[AxiStream256] =>
           warn(s"Not exactly sure what to do with tid = $tid and $tdest = $tdest for reading StreamIn of AxiStream type...")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TDATA.r := ${strm}.TDATA.r }")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TSTRB.r := ${strm}.TSTRB.r }")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TKEEP.r := ${strm}.TKEEP.r }")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TID.r := ${strm}.TID.r }")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TDEST.r := ${strm}.TDEST.r }")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TLAST := ${strm}.TLAST }")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TUSER.r := ${strm}.TUSER.r }")
+          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i) := Cat(${strm}.TUSER.r, ${strm}.TDEST.r, ${strm}.TID.r, ${strm}.TLAST.r, ${strm}.TKEEP.r, ${strm}.TSTRB.r, ${strm}.TDATA.r)}")
         case AxiStream256Bus(tid, tdest) => // If Stream was not declared as AxiStream type, assume user only cares about the tdata
           emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).r := ${strm}.TDATA.r }")
           emit(src"""${strm}.ready := ${and(ens.flatten.toSet)} & ($datapathEn) & (${strm}.TID.r === $tid.U) & (${strm}.TDEST.r === $tdest.U)""")
         case AxiStream64Bus(tid, tdest) if lhs.tp.typeArgs.head.isInstanceOf[AxiStream64] =>
           warn(s"Not exactly sure what to do with tid = $tid and $tdest = $tdest for reading StreamIn of AxiStream type...")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TDATA.r := ${strm}.TDATA.r }")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TSTRB.r := ${strm}.TSTRB.r }")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TKEEP.r := ${strm}.TKEEP.r }")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TID.r := ${strm}.TID.r }")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TDEST.r := ${strm}.TDEST.r }")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TLAST := ${strm}.TLAST }")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TUSER.r := ${strm}.TUSER.r }")
+          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i) := Cat(${strm}.TUSER.r, ${strm}.TDEST.r, ${strm}.TID.r, ${strm}.TLAST.r, ${strm}.TKEEP.r, ${strm}.TSTRB.r, ${strm}.TDATA.r)}")
         case AxiStream64Bus(tid, tdest) => // If Stream was not declared as AxiStream type, assume user only cares about the tdata
           emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).r := ${strm}.TDATA.r }")
           emit(src"""${strm}.ready := ${and(ens.flatten.toSet)} & ($datapathEn) & (${strm}.TID.r === $tid.U) & (${strm}.TDEST.r === $tdest.U)""")
         case AxiStream512Bus(tid, tdest) if lhs.tp.typeArgs.head.isInstanceOf[AxiStream512] =>
           warn(s"Not exactly sure what to do with tid = $tid and $tdest = $tdest for reading StreamIn of AxiStream type...")
-          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).TDATA.r := Cat(${strm}.TUSER.r, ${strm}.TDEST.r, ${strm}.TID.r, ${strm}.TLAST.r, ${strm}.TKEEP.r, ${strm}.TSTRB.r, ${strm}.TDATA.r)}")
+          emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i) := Cat(${strm}.TUSER.r, ${strm}.TDEST.r, ${strm}.TID.r, ${strm}.TLAST.r, ${strm}.TKEEP.r, ${strm}.TSTRB.r, ${strm}.TDATA.r)}")
         case AxiStream512Bus(tid, tdest) => // If Stream was not declared as AxiStream type, assume user only cares about the tdata
           emit(src"(0 until ${ens.length}).map{ i => ${lhs}(i).r := ${strm}.TDATA.r }")
           emit(src"""${strm}.ready := ${and(ens.flatten.toSet)} & ($datapathEn) & (${strm}.TID.r === $tid.U) & (${strm}.TDEST.r === $tdest.U)""")

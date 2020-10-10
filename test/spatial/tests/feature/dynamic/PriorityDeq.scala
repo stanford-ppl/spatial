9a5b19112dcd8516a5ab512bc15bbca15b09b8e9
mattfel <feldman.matthew1@gmail.com>
2020-10-10 10:48:34 -0700

    add conditional syntax for prioritydeq

--- test/spatial/tests/feature/dynamic/PriorityDeq.scala
+++ test/spatial/tests/feature/dynamic/PriorityDeq.scala
@@ -87,3 +87,114 @@ class PriorityDeqDynamic extends PriorityDeq(true)
 
   }
 }
+
+class MultiPriorityDeqStatic extends MultiPriorityDeq(false)
+class MultiPriorityDeqDynamic extends MultiPriorityDeq(true)
+@spatial abstract class MultiPriorityDeq(dyn: scala.Boolean) extends SpatialTest {
+  override def runtimeArgs: Args = "48"
+
+  def main(args: Array[String]): Unit = {
+    val N = ArgIn[Int]
+    setArg(N, args(0).to[Int])
+
+    val result = DRAM[Int](N*3)
+    setMem(result, Array.fill(N*3)(-1.to[Int]))
+
+    Accel {
+      // Create worker worklist queues.
+      // The priorityDeq should not deq from a lane unless the payload queue also has data, or else this causes a hang!
+      val worker1Queue = FIFO[Int](16)
+      val worker1PayloadQueue = FIFO[Int](16)
+      val worker2Queue = FIFO[Int](16)
+      val worker2PayloadQueue = FIFO[Int](16)
+      val worker3Queue = FIFO[Int](16)
+      val worker3PayloadQueue = FIFO[Int](16)
+      val doneQueue = FIFO[Int](2)
+
+      val outQueue = FIFO[Int](128)
+
+
+      if (!dyn) {
+        Stream {
+          // Producer
+          Foreach(N by 1) { i =>
+            val cmd = random[Int](6)
+            if (cmd % 3 == 0) {
+              worker1Queue.enq(0)
+              Foreach(20 by 1) { j =>
+                if (j == 19) worker1PayloadQueue.enq(j)
+              }
+            }
+            if (cmd % 3 == 1) {
+              worker2Queue.enq(1)
+              Foreach(20 by 1) { j =>
+                if (j == 19) worker1PayloadQueue.enq(j)
+              }
+            }
+            if (cmd % 3 == 2) {
+              worker3Queue.enq(2)
+              Foreach(20 by 1) { j =>
+                if (j == 19) worker1PayloadQueue.enq(j)
+              }
+            }
+          }
+
+          // Consumer
+          Foreach(N by 1) { i =>
+            val id = priorityDeq(List(worker1Queue, worker2Queue, worker3Queue), List(!worker1PayloadQueue.isEmpty, !worker2PayloadQueue.isEmpty, !worker3PayloadQueue.isEmpty))
+            val payload = if (id == 0) worker1PayloadQueue.deq() else if (id == 1) worker2PayloadQueue.deq() else if (id == 2) worker3PayloadQueue.deq() else 0.to[Int]
+            outQueue.enq(id + payload)
+          }
+        }
+      } else {
+        val kill = Reg[Bit](false)
+        Stream(breakWhen = kill).Foreach(*) { _ =>
+          // Producer
+          Pipe { // Pipe this off to prevent strangeness with the N.value + 1 orphaned primitive
+            Foreach(N.value + 1 by 1) { i =>
+              if (i < N) {
+                // Every i gets enq'ed either ONCE ore 3 TIMES
+                val cmd = random[Int](6)
+                if (cmd % 3 == 0) {
+                  worker1Queue.enq(i)
+                  Foreach(20 by 1) { j =>
+                    if (j == 19) worker1PayloadQueue.enq(j)
+                  }
+                }
+                if (cmd % 3 == 1 || cmd == 0) {
+                  worker2Queue.enq(i)
+                  Foreach(20 by 1) { j =>
+                    if (j == 19) worker1PayloadQueue.enq(j)
+                  }
+                }
+                if (cmd % 3 == 2 || cmd == 0) {
+                  worker3Queue.enq(i)
+                  Foreach(20 by 1) { j =>
+                    if (j == 19) worker1PayloadQueue.enq(j)
+                  }
+                }
+              }
+              retimeGate()
+              doneQueue.enq(-1, i >= N.value)
+            }
+          }
+
+          // Consumer
+          Foreach(*) { i =>
+            val id = priorityDeq(List(worker1Queue, worker2Queue, worker3Queue, doneQueue), List(!worker1PayloadQueue.isEmpty, !worker2PayloadQueue.isEmpty, !worker3PayloadQueue.isEmpty, Bit(true)))
+            val payload = if (id == 0) worker1PayloadQueue.deq() else if (id == 1) worker2PayloadQueue.deq() else if (id == 2) worker3PayloadQueue.deq() else 0.to[Int]
+            if (id != -1) outQueue.enq(id + payload)
+            else kill := Bit(true)
+          }
+        }
+      }
+
+      result(0::outQueue.numel) store outQueue
+    }
+
+    val got = getMem(result)
+    printArray(got, "Got data:")
+    assert(true, "Just want the app to not hang")
+
+  }
+}

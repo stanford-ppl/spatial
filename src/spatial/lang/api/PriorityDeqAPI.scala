37d3204628f183804032330acedb954aaacec40c
mattfel <feldman.matthew1@gmail.com>
2020-10-10 10:50:47 -0700

    typo

--- src/spatial/lang/api/PriorityDeqAPI.scala
+++ src/spatial/lang/api/PriorityDeqAPI.scala
@@ -20,7 +20,7 @@ trait PriorityDeqAPI {  this: Implicits =>
     val gid = ctx.line // TODO: this is probably an unsafe way to compute a group id
     val datas: Seq[T] = fifo.zipWithIndex.map{case (f,i) =>
       // The enable should also have a !f.isEmpty but this messes up II analysis so it is added at codegen as a hack for this deq node
-      val d = stage(FIFOPriorityDeq(f, Set(fifo.take(i).map(_.isEmpty).fold(Bit(true), cond(i)){case (a: Bit,b: Bit) => a && b})))
+      val d = stage(FIFOPriorityDeq(f, Set(fifo.take(i).map(_.isEmpty).fold(Bit(true)), cond(i)){case (a: Bit,b: Bit) => a && b})))
       d
     }
     val x = stage(PriorityMux[T](fifo.map(!_.isEmpty), datas.map{x => boxBits[T](x)}))

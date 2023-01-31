13b0f41dbfcc0fb57f0c163953abf065142372b6
nzhang <stanfurd@stanford.edu>
2023-01-31 10:52:37 -0800

    Added buffering to stream files.

--- utils/src/utils/io/stream.scala
+++ utils/src/utils/io/stream.scala
@@ -1,11 +1,13 @@
 package utils.io
 
-import java.io.PrintStream
-import java.nio.file.{Files,Paths}
+import java.io.{BufferedOutputStream, FileOutputStream, PrintStream}
+import java.nio.file.{Files, Paths}
 
 object stream {
   def createStream(dir: String, filename: String): PrintStream = {
     Files.createDirectories(Paths.get(dir))
-    new PrintStream(dir + files.sep + filename)
+    val fname = dir + files.sep + filename
+    val outputStream = new BufferedOutputStream(new FileOutputStream(fname), 1 << 16)
+    new PrintStream(outputStream)
   }
 }
